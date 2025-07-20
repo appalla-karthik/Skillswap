daf143411286aa1d2ec75abbc14f47347aa698e3
appalla-karthik <appallakarthik07@gmail.com>
2025-07-21 01:43:47 +0530

    Update settings.py

--- skillswap_project/skillswap_project/settings.py
+++ skillswap_project/skillswap_project/settings.py
@@ -61,17 +61,10 @@ if os.getenv('RENDER'):
     }
 else:
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'swapit',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-    }
+}
 
 # Password Validators
 AUTH_PASSWORD_VALIDATORS = [
