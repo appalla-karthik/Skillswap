ccbda01d35f1abf0bf1bc4dda06a3a030b3cf8b0
appalla-karthik <appallakarthik07@gmail.com>
2025-07-21 02:23:42 +0530

    Update settings.py

--- skillswap_project/skillswap_project/settings.py
+++ skillswap_project/skillswap_project/settings.py
@@ -54,12 +54,14 @@ TEMPLATES = [
     },
 ]
 
-# Database
-
-    DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swapit',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
